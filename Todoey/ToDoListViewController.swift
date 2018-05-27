87a4571ffcead2b43add53bb6ff10362a78bd8c8
ravizza <ravizza@ravizzas-imac-pro.local>
2018-05-27 16:52:40 -0700

    To do list with no persistent data.

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by ravizza on 5/27/18.
+//  Copyright © 2018 ravizza. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["A", "B", "C"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+        
+    }
+    
+    // MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+       
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+        }
+        else {
+            cell?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
